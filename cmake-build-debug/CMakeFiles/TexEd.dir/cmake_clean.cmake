789fc1388e249b8f26aa74b74e34a3cac5a1dab3
Emirhan Kılıç <emirhan.kilic@ug.bilkent.edu.tr>
2024-06-29 12:52:47 +0300

    .

---
+++ cmake-build-debug/CMakeFiles/TexEd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TexEd.dir/main.c.o"
+  "CMakeFiles/TexEd.dir/main.c.o.d"
+  "TexEd"
+  "TexEd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TexEd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
